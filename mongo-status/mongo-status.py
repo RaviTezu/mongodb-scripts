ac915543bb154c78a9fb3b51ddc4d8ca2ad231ef
RaviTeja <rpothan@corp.untd.com>
2014-04-01 17:21:05 +0530

    Tweaks

--- mongo-status/mongo-status.py
+++ mongo-status/mongo-status.py
@@ -17,16 +17,32 @@ def mongos_connect(host, port):
         print "Unable to connect to the mongo host"
         print e
 
+##grep replica sets from shards_info
+def getReplicas(shards):
+    replicas = {}
+    for rs in shards:
+        rs_name = rs['_id']
+        rs_mems = rs['host'].split('/')[1]
+        replicas[rs_name] = rs_mems
+    return replicas
+
+##prints replica sets:
+def printReplicas(reps):
+    for k, v in sorted(reps.iteritems()):
+        print k + " : " + v
+
 ##main function:
 def main():
     ## config_data.ini file location, as it is in the same directory just the filename is enough. 
     default_config_file = 'config_data.ini'
     parser = SafeConfigParser()
     parser.read(default_config_file)
-    mongos_host = parser.get('default', 'mongos_host')
-    mongos_port = parser.get('default', 'mongos_port')
+    mongos_host = str(parser.get('default', 'mongos_host'))
+    mongos_port = int(parser.get('default', 'mongos_port'))
     shards_info = mongos_connect(mongos_host, mongos_port)
-    print shards_info
+    #get replica sets info.
+    rsets = getReplicas(shards_info)
+    printReplicas(rsets)
 
 
 if __name__ == '__main__':
